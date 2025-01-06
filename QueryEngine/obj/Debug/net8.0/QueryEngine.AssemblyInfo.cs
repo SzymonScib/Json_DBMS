615024f30643397e3da41e38475feedd3d414edf
SzymonScib <szymonscibcs@gmail.com>
2025-01-06 17:23:48 +0100

    Update BTreeTests

--- QueryEngine/obj/Debug/net8.0/QueryEngine.AssemblyInfo.cs
+++ QueryEngine/obj/Debug/net8.0/QueryEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueryEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4367f9a5f250b730a4aa53cb24aaa42ed26a8cc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6077122c955b491482a29f9f04eb10f1746e9750")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueryEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueryEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
